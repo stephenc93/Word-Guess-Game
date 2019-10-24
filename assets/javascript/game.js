63e937f83b32f1b31fcb68212dcd2af2968e31f1
Stephen Caceres <54186983+stephenc93@users.noreply.github.com>
2019-10-23 22:57:32 -0400

    finishing touches, minor bugs remain

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,181 @@
-// JavaScript function that wraps everything
-$(document).ready(function () {
-    
-    let maxGuess = 15;
-    let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    let songNames = ["pushit to the limit", "im hot tonight", "shes on fire"]
+document.addEventListener("DOMContentLoaded", function () {
 
-    let songNames = songNames[Math.floor(Math.random() * songNames.length)];
+    let answers = ["imhottonight", "pushittothelimit", "shesonfire"];
+    let currentAnswer = "";
+    let lettersOfAnswer = [];
+    let blanks = 0;
+    let blanksAndCorrect = [];
+    let wrongGuess = [];
+    let wins = 0;
+    let losses = 0;
+    let guessesRemaining = 12;
+    let cycle = 0;
 
-    let answerArray = [];
-    u for (let i = 0; i < songNames.length; i++) {
-        answerArray[i] = "_";
+    function Game() {
+        currentAnswer = answers[cycle];
+        cycle++;
+        lettersOfAnswer = currentAnswer.split("");
+        blanks = lettersOfAnswer.length;
+        for (let i = 0; i < blanks; i++) {
+            blanksAndCorrect.push("_");
+        }
+        document.getElementById("currentWord").innerHTML = "  " + blanksAndCorrect.join("  ");
     }
-    let remainingLetters = songNames.length;
 
-    // Gets Link for Intro Song
-    const audioElement = document.createElement("audio");
-    audioElement.setAttribute("src", "assets/intro.mp3");
+    let introTheme = document.getElementById("intro");
+    let imHot = document.getElementById("im-hot-tonight");
+    let pushIt = document.getElementById("push-it-to-the-limit");
+    let onFire = document.getElementById("shes-on-fire");
+    let ginasTheme = document.getElementById("ginas-theme");
+    let tonysTheme = document.getElementById("tonys-theme");
 
-    // Intro Buttons
-    $(".theme-button").on("click", function () {
-        audioElement.play();
-    });
-    $(".pause-button").on("click", function () {
-        audioElement.pause();
-    });
+    function intro() {
+        imHot.pause();
+        pushIt.pause();
+        onFire.pause();
+        introTheme.play();
+    } 
 
-    let guess = prompt("Guess a letter, or click Cancel to stop playing.");
-    v if (guess === null) {
-    break;
-    w } else if (guess.length !== 1) {
-        alert("Please enter a single letter.");
-        } else {
-x // Update the game state with the guess
+    function pauseIntro() {
+        imHot.pause();
+        pushIt.pause();
+        onFire.pause();
+        introTheme.pause();
+    }
+
+    function outroTheme() {
+        introTheme.pause();
+        imHot.pause();
+        pushIt.pause();
+        onFire.pause();
+        if (wins >= 2) {
+            tonysTheme.pause();
+            ginasTheme.play();
+        }
+        else if (losses >= 2) {
+            ginasTheme.pause();
+            tonysTheme.play();
+        }
+    }
+
+    function pauseOutro() {
+        introTheme.pause();
+        imHot.pause();
+        pushIt.pause();
+        onFire.pause();
+        ginasTheme.pause();
+        tonysTheme.pause();
+    }
+
+    document.addEventListener('click', function (event) {
+        if (!event.target.matches('.theme-button')) return;
+        event.preventDefault();
+        intro();
+    }, false);
+
+    document.addEventListener('click', function (event) {
+        if (!event.target.matches('.pause-button')) return;
+        event.preventDefault();
+        pauseIntro();
+    }, false);
+
+    document.addEventListener('click', function (event) {
+        if (!event.target.matches('.outro-button')) return;
+        event.preventDefault();
+        outroTheme();
+    }, false);
+
+    document.addEventListener('click', function (event) {
+        if (!event.target.matches('.outro-pause-button')) return;
+        event.preventDefault();
+        pauseOutro();
+    }, false);
+
+    function aud() {
+        if (currentAnswer === answers[0]) {
+            introTheme.pause();
+            pushIt.pause();
+            onFire.pause();
+            imHot.play();
+        }
+        else if (currentAnswer === answers[1]) {
+            introTheme.pause();
+            imHot.pause();
+            onFire.pause();
+            pushIt.play();
+        }
+        else if (currentAnswer === answers[2]) {
+            introTheme.pause();
+            imHot.pause();
+            pushIt.pause();
+            onFire.play();
+        }
+    };
+
+    function reset() {
+        guessesRemaining = 12;
+        wrongGuess = [];
+        blanksAndCorrect = [];
+        if (wins !== 2 || losses !== 2) {
+            Game();
+        }
+    }
+
+    function checkLetters(letter) {
+        let letterInWord = false;
+        for (let i = 0; i < blanks; i++) {
+            if (currentAnswer[i] == letter) {
+                letterInWord = true;
+            }
+        }
+        if (letterInWord) {
+            for (let i = 0; i < blanks; i++) {
+                if (currentAnswer[i] == letter) {
+                    blanksAndCorrect[i] = letter;
+                }
+            }
+        }
+        else {
+            wrongGuess.push(letter);
+            guessesRemaining--;
+        }
+    }
+
+    function complete() {
+        if (lettersOfAnswer.toString() == blanksAndCorrect.toString()) {
+            wins++;
+            reset()
+            document.getElementById("winDiv").innerHTML = " " + wins;
+        } 
+        else if (guessesRemaining === 0) {
+            losses++;
+            reset()
+            document.getElementById("lossDiv").innerHTML = " " + losses;
+        }
+        document.getElementById("currentWord").innerHTML = "  " + blanksAndCorrect.join(" ");
+        document.getElementById("guessesDiv").innerHTML = " " + guessesRemaining;
+    }
+
+    Game();
+
+    document.onkeyup = function (event) {
+        let guesses = String.fromCharCode(event.keyCode).toLowerCase();
+        aud();
+        checkLetters(guesses);
+        complete();
+        document.getElementById("wrongLetters").innerHTML = "  " + wrongGuess.join(" ");
+        victoryOrDefeat();
 }
 
+    function victoryOrDefeat() {
+        if (wins >= 2) {
+            onFire.pause();
+            outroTheme();
+        }
+        else if (losses >= 2) {
+            onFire.pause();
+            outroLoss();
+        }
+    }
 
-    // pick a song from array in order 
-
-    // While the word has not been guessed {
-    //     Show the player their current progress
-    //     Get a guess from the player
-    //     If the player wants to quit the game {
-    //     Quit the game
-    //     }
-    //     Else If the guess is not a single letter {
-    //     Tell the player to pick a single letter
-    //     }
-    //     Else {
-    //     If the guess is in the word {
-    //     Update the player's progress with the guess
-    //     }
-    //     }
-    //    }
-    // Congratulate the player on guessing the word
-
-
-
-
-    // Gets Link for Outro Song
-
-    // if player wins > 1 game, play ginastheme
-    // else play tonystheme
-    // audioElement = document.createElement("audio");
-    // audioElement.setAttribute("src", "placeholder");
-
-    // Outro Buttons
-    $(".outro-button").on("click", function () {
-        audioElement.play();
-    });
-    $(".outro-pause-button").on("click", function () {
-        audioElement.pause();
-    });
 });
